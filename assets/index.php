2558d5913a9385e3363ab0ef83d0ed2a706e9879
Ricardo Buitenhuis <meowoo@live.nl>
2019-01-21 12:56:53 +0100

    animations

---
+++ assets/index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>FitAssistance</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="google-site-verification" content="lmLr-HBvJoXEUOr3XZ_rzlnXMs6H8O8kwYWq5amxyEM" />
+
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+		<link rel="icon" href="images/fiticon.ico">
+
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/header.css" />
+		<link rel="stylesheet" href="assets/css/banner.css" />
+		<link rel="stylesheet" href="assets/css/one.css" />
+		<link rel="stylesheet" href="assets/css/two.css" />
+		<link rel="stylesheet" href="assets/css/three.css" />
+		<link rel="stylesheet" href="assets/css/four.css" />
+		<link rel="stylesheet" href="assets/css/five.css" />
+		<link rel="stylesheet" href="assets/css/footer.css" />
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="assets/js/smoothslide.js"></script>
+
+	</head>
+
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<img src="images/fitlogo.jpg" alt="" class="logo">
+					<nav id="nav">
+						<a href="#one">Home</a>
+						<a href="#two">Hoe werkt het?</a>
+						<a href="#three">Trainingen</a>
+						<a href="#four">Over ons</a>
+						<a href="#five">Privacy / Contact</a>
+						<a href="#footer">Contactformulier</a>
+
+						<div class="container" onclick="myFunction()" id="container">
+						  <div class="bar1"></div>
+						  <div class="bar2"></div>
+						  <div class="bar3"></div>
+						</div>
+					</nav>
+				</div>
+			</header>
+
+			<!-- Top Navigation Menu -->
+			<div class="topnav">
+			  <!-- Navigation links (hidden by default) -->
+			  <div id="myLinks">
+					<a href="#one" onclick="myFunction()">Home</a>
+					<a href="#two" onclick="myFunction()">Hoe werkt het?</a>
+					<a href="#three" onclick="myFunction()">Trainingen</a>
+					<a href="#four" onclick="myFunction()">Over ons</a>
+					<a href="#five" onclick="myFunction()">Privacy / Contact</a>
+					<a href="#footer" onclick="myFunction()">Contactformulier</a>
+			  </div>
+			</div>
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<h1> <span>The body achieves, <br />
+					what the mind believes</span></h1>
+					<ul class="actions">
+						<li><a href="#footer" class="button alt">Contact</a></li>
+					</ul>
+				</div>
+			</section>
+
+		<!-- One -->
+			<section id="one">
+				<div class="inner">
+					<header>
+
+						<h2>Home</h2>
+					</header>
+					<p>Welkom bij Fit Assistance! Bij Fit Assistance hebben wij één doel: mensen helpen nóg beter te worden dan dat ze al zijn. Wij bieden mentale ondersteuning bij je gezondheidsdoelen.
+						Heb je moeite met het volhouden van een sportprogramma of een bepaald dieet? Wil je graag beter worden in de sport die je beoefent? Of wil je meer routine opbouwen en je slechte
+						gewoontes veranderen? Neem dan contact op voor een vrijblijvend kennismakingsgesprek.</p>
+				</div>
+
+			</section>
+
+			<!-- Two -->
+				<section id="two">
+					<div class="inner">
+						<header>
+							<h2>Hoe werkt het?</h2>
+						</header>
+							<p>Je kan met ons in contact komen via het contactformulier, door een mailtje te sturen of te bellen/appen. We maken dan een afspraak voor een telefonisch gesprek. Tijdens dit
+								gesprek bespreken we je situatie en kijken we of Fit Assistance daarbij aansluit. Daarna vinden de afspraken plaats in de buitenlucht tijdens een wandeling. De locatie hiervan
+								bepalen we samen tijdens het telefoongesprek. De gesprekken duren ongeveer driekwartier. Na elk gesprek krijg je een mail met daarin de hoofdpunten die besproken zijn, zodat je
+								dit nog even rustig door kan lezen.</p>
+							<p>Wachttijd: geen.</p>
+						  <p>Tarieven:<br>
+								Eerste gesprek (telefonisch) &nbsp  &nbsp &nbsp &nbsp Gratis<br>
+								Coachingssessie&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp € 30,- </p>
+							<p>Let op: de sessies worden niet vergoed door je zorgverzekeraar.</p>
+					</div>
+				</section>
+
+				<!-- Banner2 -->
+					<section id="banner2">
+						<h3>&zwnj;</h3><h3>&zwnj;</h3>
+					</section>
+
+					<!-- Three -->
+						<section id="three">
+							<div class="inner">
+								<header>
+									<h2>Trainingen</h2>
+								</header>
+									<p>Ben je op zoek naar een manier om de vitaliteit en de prestaties van je werknemers te verbeteren? Maak een afspraak om samen de mogelijkheden voor een training op locatie
+										te bespreken.</p>
+							</div>
+							<div class="row">
+								<div class="column">
+									<h4>Hoe leer ik een nieuwe gewoonte?</h4>
+									<p>Leer hoe gewoontes ontstaan en waarom</p><hr>
+									<p>Krijg inzicht in eigen gewoontes</p><hr>
+									<p>Verbeter vitaliteit door slechte gewoontes te vervangen voor goede</p><hr>
+									<p>Krijg concrete handvatten voor het ontwikkelen van routine</p>
+									<ul class="scroll">
+										<li><a href="#footer" class="button alt">Neem contact op</a></li>
+									</ul>
+								</div>
+								<div class="column">
+									<h4>Hoe sta ik mezelf niet in de weg?</h4>
+									<p>Krijg inzicht in obstakels die ontwikkeling in de weg staan</p><hr>
+									<p>Leer valkuilen te herkennen en te vermijden</p><hr>
+									<p>Laat niets in de weg staan van het bereiken van je doel</p>
+
+									<ul class="scroll">
+										<li><a href="#footer" class="button alt">Neem contact op</a></li>
+									</ul>
+								</div>
+								<div class="column">
+									<h4>Training op maat</h4>
+									<p>Wat wil je graag bereiken met de training?</p><hr>
+									<p>Gebaseerd op jouw behoeftes</p>
+
+									<ul class="scroll">
+										<li><a href="#footer" class="button alt">Neem contact op</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+
+		<!-- Four -->
+			<section id="four">
+				<div class="inner">
+					<article>
+						<div class="content">
+							<header>
+								<h3>Over ons</h3>
+							</header>
+							<div class="image fit">
+								<img src="images/pic01.jpg" alt="" />
+							</div>
+							<p>Hallo! Mijn naam is Thije van Lokven, ik ben Toegepast Psycholoog in opleiding. Momenteel ben ik bezig met mijn laatste jaar. Ik heb een grote passie voor sport en de
+								mentale aspecten die daarbij komen kijken. Momenteel ben ik bezig met mijn laatste jaar. Vandaar mijn keuze voor de minor prestatiepsychologie. Gedurende mijn stageperiode
+								bij een sportschool heb ik ervaring opgedaan in het mentaal begeleiden van sporters. Nadat ik zag wat voor effect de coaching had, kreeg ik de droom om Fit Assistance op te richten.
+								Mijn doel: zoveel mogelijk mensen helpen hun sportdoelen te bereiken.</p>
+						</div>
+					</article>
+					<article class="alt">
+						<div class="content">
+							<header>
+								<h3>&zwnj;</h3>
+							</header>
+							<div class="image fit">
+								<img src="images/pic02.jpg" alt="" />
+							</div>
+							<p>Hi! Ik ben Alma Elshof, mede-eigenaar van Fit Assistance. Mijn passie: cijfertjes. Ik zorg ervoor dat alles ‘achter de schermen’ goed loopt, zodat Thije zich voor de volle 100% kan
+								richten op het coachen. Mijn taken zijn onder andere het organiseren van de agenda, de bedrijfsadministratie en de marketing voor Fit Assistance.</p>
+						</div>
+					</article>
+				</div>
+			</section>
+
+		<!-- Five -->
+			<section id="five">
+				<div class="inner">
+					<article>
+						<div class="content">
+							<i class="fas fa-lock"></i>
+							<header>
+								<h3>Privacy</h3>
+							</header>
+							<p>Alles wat besproken wordt tijdens de gesprekken blijft tussen jou en je coach. Je situatie wordt niet besproken met anderen, ook niet met collega’s.</p>
+						</div>
+					</article>
+					<article>
+					<div class="content">
+							<i class="fas fa-user"></i>
+							<header>
+								<h3>Contact</h3>
+							</header>
+							<p>info@fitassistance.nl<br>
+								Thije 06-12549099<br>
+								Alma 06-11526224<br><br>
+								KvK-nummer: 73000574<br>
+								BTW nummer: NL859315472B01<br>
+								IBAN: NL15 KNAB 0257 9986 83</p>
+						</div>
+					</article>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<section id="footer">
+				<div class="inner">
+					<header>
+						<h2>Contactformulier</h2>
+					</header>
+					<form method="post" action="https://formspree.io/info@fitassistance.nl">
+						<div class="field half first">
+							<label for="name">Name</label>
+							<input type="text" name="name" id="name" />
+						</div>
+						<div class="field half">
+							<label for="email">Email</label>
+							<input type="text" name="email" id="email" />
+						</div>
+						<div class="field">
+							<label for="message">Message</label>
+							<textarea name="message" id="message" rows="6"></textarea>
+						</div>
+						<ul class="actions">
+							<li><input type="submit" name="submit" value="Send Message" class="alt" /></li>
+						</ul>
+					</form>
+					<div class="copyright">
+						<a href="https://www.facebook.com/fitassistance" class="fab fa-facebook-square"></a>
+						<a href="https://www.linkedin.com/company/fit-assistance/" class="fab fa-linkedin"></a>
+						<a href="https://www.instagram.com/fitassistance.nl/" class="fab fa-instagram"></a>
+					</div>
+				</div>
+			</section>
+
+	</body>
+</html>
--- assets/js/smoothslide.js
+++ assets/js/smoothslide.js
@@ -32,21 +32,21 @@ function myFunction() {
 	function myFunction2() {
   var y = document.getElementById("myLinks");
   if (y.style.display === "block") {
-    y.style.display = "none";
-		y.style.border
+    //y.style.display = "none";
+		    $("#myLinks").slideToggle("fast");
   } else {
-    y.style.display = "block";
+    //y.style.display = "block";
 		y.style.borderBottom = "4px solid #010101";
+$("#myLinks").slideToggle("fast");
   }
 }
 
-$("button").click(function(){
-  $("#div1").fadeIn();
-  $("#div2").fadeIn("slow");
-  $("#div3").fadeIn(3000);
+$(document).ready(function(){
+	if ($(window).width() > 980)
+	$('#banner .inner h1').animate({paddingLeft: "0"},2500,);
 });
 
-/*$(window).scroll(function(){
+$(window).scroll(function(){
 		// This is then function used to detect if the element is scrolled into view
 		function elementScrolled(elem)
 		{
@@ -57,8 +57,25 @@ $("button").click(function(){
 		}
 
 		// This is where we use the function to detect if ".box2" is scrolled into view, and when it is add the class ".animated" to the <p> child element
-		if(elementScrolled("#one")) {
-			$('#one .inner').animate({marginLeft: "16%"},2000,);
-			console.log("yes");
+		if(elementScrolled("#one .inner")) {
+			if ($(window).width() > 1300)
+				$('#one').animate({paddingTop: "6em", paddingRight: "0", paddingBottom: "4em", paddingLeft: "0"},1000,);
+			}
+
+		if(elementScrolled("#two .inner")) {
+			if ($(window).width() > 1300)
+			$('#two').animate({paddingTop: "6em", paddingRight: "0", paddingBottom: "4em", paddingLeft: "0"},1000,);
 		}
-});*/
+		if(elementScrolled("#three .row")) {
+			if ($(window).width() > 1300)
+			$('#three .row').animate({paddingLeft: "0", paddingRight: "0"},1000,);
+		}
+		if(elementScrolled("#five .inner")) {
+			if ($(window).width() > 1300)
+			$('#five').animate({paddingTop: "6em", paddingRight: "0", paddingBottom: "4em", paddingLeft: "0"},1000,);
+		}
+		if(elementScrolled("#four .inner")) {
+			if ($(window).width() > 1300)
+			$('#four .content').animate({paddingRight: "0", paddingLeft: "0"},1000,);
+		}
+});
